0cf6184652b21de94edbe364c2918aa05e0ac98b
Kassandra <K1995Moreno@gmail.com>
2022-11-06 16:39:32 -0500

    commit tree e63c20f2b9e4eedcc681b87da1ff55275f253cac

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Add your code here
+
+
+// const configurationObject= {
+//     method: "POST",
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'},
+//     body: JSON.stringify({
+//         dogName: 'Bryon',
+//         dogBreed: 'Poodle'
+// })}
+// fetch('http://localhost:3000/dogs',configurationObject)
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(object){
+//         console.log(object)
+//     })
+//     .catch(function(error){
+//         alert('Freak out, something went wrong')
+//         console.log(error.message)
+//     })
+
+
+function submitData() {
+    const posting= {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'},
+        body: JSON.stringify({
+            name: 'Kasey',
+            email: 'aol',
+        })
+    }
+    return posting
+}   
+fetch('http://localhost:3000/users',submitData)
+   .then(function(response) {
+        return response.json()
+    })
+    .then(function(object){
+        console.log(object)
+    })
+    .catch(function(error) {
+        alert('submitData error alert')
+        console.log(error.message)
+    })
+
+
+
+
+
